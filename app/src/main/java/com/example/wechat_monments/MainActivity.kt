1fbb98c09df166b7e58927c07f85d478975a2705
Leqi <michaelleqihust@gmail.com>
2020-11-04 15:44:28 +0800

    [leqi] feat: Config project with Card #1 tasking. 1. Git 2. Network permission 3. SwipeRefreshLayout

--- app/src/main/java/com/example/wechat_monments/MainActivity.kt
+++ app/src/main/java/com/example/wechat_monments/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.wechat_monments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initComponents()
+    }
+
+    private fun initComponents() {
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout)
+
+        swipeRefreshLayout.setOnRefreshListener {
+            swipeRefreshLayout.isRefreshing = false
+        }
     }
 }
